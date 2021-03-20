9a8990a801d5b2c2fe1f0dbb9ff55c739db7c332
mavozd <mavozd@ttu.ee>
2021-03-20 23:17:27 +0200

    Local storage

--- scripts.js
+++ scripts.js
@@ -1,11 +1,31 @@
 var entered_values = [];
 var filtered_values = [];
 
+var localStorage = Window.localStorage;
+
 var text_area = document.getElementById("tags");
 var add_button = document.getElementById("add");
 var edit_button = document.getElementById("edit");
 var save_button = document.getElementById("save");
 var tag_list = document.getElementsByClassName("tag ");
+
+
+store_tags_in_local_storage = () => {
+    let tag_display = document.getElementsByClassName("tag-display")[0];
+    console.log(tag_display.innerHTML);
+    localStorage.setItem('tags', tag_display.innerHTML);
+}
+
+
+if (localStorage.getItem('tags')){
+    let tag_display = document.getElementsByClassName("tag-display")[0];
+    tag_display.innerHTML = localStorage.getItem('tags');
+    let deleteButtons = document.getElementsByClassName("delete");
+    for(let i=0; i < deleteButtons.length; i++){
+        add_event_listener_to_delete_btn(deleteButtons[i]);
+    }
+}
+
 
 change_edit_button_state = () => {
     if (tag_list.length == 0) {
@@ -13,13 +33,17 @@ change_edit_button_state = () => {
     } else {
         edit_button.disabled = false;
     }
-    console.log(edit_button);
 }
+
+
+change_edit_button_state();
+
 
 text_area.addEventListener('keydown', e => {
     if (e.code === "Enter") {
         create_new_tags();
         change_edit_button_state();
+        store_tags_in_local_storage();
     }
 });
 
@@ -31,6 +55,8 @@ save_button.addEventListener('click', e => {
     save_button.classList.remove("active");
     edit_button.innerHTML = "Edit";
     change_edit_button_state();
+    store_tags_in_local_storage();
+    add_button.disabled = false;
 });
 
 
@@ -48,11 +74,14 @@ edit_button.addEventListener('click', e => {
     text_area.value = tag_string;
     save_button.classList.add("active");
     edit_button.innerHTML = "Reset";
+    add_button.disabled = true;
 });
 
 add_button.addEventListener('click', e => {
+    localStorage.setItem("message", "hello from storage");
     create_new_tags();
     change_edit_button_state();
+    store_tags_in_local_storage();
 });
 
 create_new_tags = () => {
@@ -96,10 +125,7 @@ create_tag_node = (element) => {
     delete_btn.classList.add("delete");
     delete_btn.type="button";
 
-    delete_btn.addEventListener('click', e => {
-        delete_btn.parentElement.remove();
-        change_edit_button_state();
-    })
+    add_event_listener_to_delete_btn(delete_btn);
 
     let tag_div = document.createElement("div");
     tag_div.classList.add("tag");
@@ -111,5 +137,13 @@ create_tag_node = (element) => {
     tag_div.innerHTML = element;
     tag_div.appendChild(delete_btn);
     return tag_div;
+}
+
+function add_event_listener_to_delete_btn(delete_btn) {
+    delete_btn.addEventListener('click', e => {
+        delete_btn.parentElement.remove();
+        change_edit_button_state();
+        store_tags_in_local_storage();
+    });
 }
 
